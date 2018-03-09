549e497e64f7cd8f37065ae41166cb5cdac3e630
Gabriel Schmitt <gabrielsch@outlook.com>
2018-03-09 14:03:34 -0300

    Add prettierrc, bump versions

---
+++ packages/eslint-config-backend/prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  singleQuote: false,
+  noSemi: false,
+  parser: "flow",
+};
---
+++ packages/eslint-config-frontend/prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  singleQuote: false,
+  noSemi: false,
+  parser: "babylon",
+};
